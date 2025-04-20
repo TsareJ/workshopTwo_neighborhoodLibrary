e5b50e31eb46cc939fdf3f706bcdd7013c9158d7
TsareJ <tsarejoseph@gmail.com>
2025-04-20 17:06:39 -0400

    getters and setters applied

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -7,8 +7,55 @@ public class Main {
     private boolean isCheckedOut;
     private String checkedOutTo;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
     public Main(int id, String isbn, String title) {
+
+            this.id = id;
+            this.isbn = isbn;
+            this.title = title;
+            this.isCheckedOut = false;
+            this.checkedOutTo = " ";
 
 
     }
+
+    public void chec
 }
