639895dd6e4549142235215a681e6507af001d32
TsareJ <tsarejoseph@gmail.com>
2025-04-20 16:45:47 -0400

    Began to design book class and inputting constructor

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,14 @@
+package com.ps;
+
+public class Main {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    public Main(int id, String isbn, String title) {
+
+
+    }
+}
